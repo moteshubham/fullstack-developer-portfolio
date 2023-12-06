8dde0f58d6c51f8c071b322c2980394a581bcbbd
Shubham Mote <moteshubham@gmail.com>
2023-12-07 01:15:12 +0530

    up

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+
+import React, { useRef } from 'react';
 import './contact.css'
+
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerFill} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+
+// import emailjs from 'emailjs-com'
+
+
 const Contact = () => {
+  const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+  //     .then((result) => {
+  //         console.log(result.text);
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+
+  //   e.target.reset();
+  // };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>tutorial@gmail.com</h5>
+            <a href="mailto:tutorial@gmail.com">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerFill className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Name</h5>
+            <a href="http://m.me/name" rel='noreferrer' target='_blank'>Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>Whatsapp</h4>
+            <h5>+123456789</h5>
+            <a href="http://api.whatsapp.com/send?phone+123456789" rel='noreferrer' target='_blank'>Send a message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Email' required />
+          <textarea name="message" rows="17" placeholder='Your Message' required></textarea>
+          <button type='sumbit' className='btn btn-primary'>Send a Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
